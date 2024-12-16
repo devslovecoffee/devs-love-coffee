2ef2e3517c443b728f488ab5d868e5d097cd55c8
Peter Bokor <petttr1@gmail.com>
2024-12-16 08:27:16 +0100

    tidy up stuff within repo, minor changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		excerpt: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		image: z.string().optional(),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    excerpt: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    image: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
